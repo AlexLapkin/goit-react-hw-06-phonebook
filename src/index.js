5a496becce27eded977dd16673e78864ba1ea49e
AlexLapkin <130095964+AlexLapkin@users.noreply.github.com>
2023-11-18 17:17:23 +0200

    upd

--- src/index.js
+++ src/index.js
@@ -7,11 +7,11 @@ import { Provider } from 'react-redux';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  //<React.StrictMode>
     <Provider store={store}>
       <PersistGate persistor={persistor}>
         <App />
         </PersistGate>
       </Provider>
-    </React.StrictMode>
+   // </React.StrictMode>
 );
